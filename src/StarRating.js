2a4aa1983a6579398b3b8de38536e042a8fa24bf
Joary13 <jolea13008@gmail.com>
2023-12-05 15:32:28 +0100

    config component star prop

--- src/StarRating.js
+++ src/StarRating.js
@@ -10,44 +10,66 @@ const startContainerStyle = {
   display: 'flex',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
-};
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = '#fcc419',
+  size = '48',
+  className = '',
+  message = [],
+  defaultRating = 0,
+  onMovieRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
-  // function handdleHover() {
-  //   () => setHover(i + 1);
-  // }
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    fontSize: `${size / 1.5}px`,
+    color,
+  };
+
+  function handdleRating(rating) {
+    setRating(rating);
+    onMovieRating(rating);
+  }
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={startContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i + 1}
+            color={color}
+            size={size}
             full={tempRating ? tempRating > i : rating > i}
-            onClick={() => setRating(i + 1)}
+            onClick={() => {
+              handdleRating(i + 1);
+            }}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ''}</p>
+      <p style={textStyle}>
+        {message.length === maxRating
+          ? tempRating
+            ? message[tempRating - 1]
+            : message[rating - 1]
+          : tempRating || rating || ''}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  width: '48px',
-  height: '48px',
-  display: 'block',
-  cursor: 'pointer',
-};
+function Star({ onClick, onHoverIn, onHoverOut, color, size, full }) {
+  const starStyle = {
+    width: `${size / 1.5}px`,
+    height: `${size / 1.5}px`,
+    display: 'block',
+    cursor: 'pointer',
+  };
 
-function Star({ onClick, onHoverIn, onHoverOut, full }) {
   return (
     <span
       role='button'
@@ -60,8 +82,8 @@ function Star({ onClick, onHoverIn, onHoverOut, full }) {
         <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 20 20'
-          fill='#000'
-          stroke='#000'
+          fill={color}
+          stroke={color}
         >
           <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
         </svg>
@@ -70,7 +92,7 @@ function Star({ onClick, onHoverIn, onHoverOut, full }) {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke='#000'
+          stroke={color}
         >
           <path
             strokeLinecap='round'
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
@@ -7,7 +7,25 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      color='blue'
+      size='24'
+      className='test'
+      message={['horrible', 'bad', 'ok', 'good', 'super']}
+    />
+    <Test />
+    <StarRating maxRating={10} defaultRating={2} />
   </React.StrictMode>
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating maxRating={7} onMovieRating={setMovieRating} />
+      <p>this movie is a {movieRating} star rating</p>
+    </div>
+  );
+}
