072cbb231e4972aad890748837d3ee979677cbdc
Joary13 <jolea13008@gmail.com>
2023-12-06 15:02:52 +0100

    initialization textExpander

--- src/index.js
+++ src/index.js
@@ -2,30 +2,39 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
-import StarRating from './StarRating';
+// import StarRating from './StarRating';
+import App from './Text.js';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <StarRating
-      maxRating={5}
-      color='blue'
-      size='24'
-      className='test'
-      message={['horrible', 'bad', 'ok', 'good', 'super']}
-    />
-    <Test />
-    <StarRating maxRating={10} defaultRating={2} />
+    <App />
   </React.StrictMode>
 );
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// root.render(
+//   <React.StrictMode>
+//     <StarRating
+//       maxRating={5}
+//       color='blue'
+//       size='24'
+//       className='test'
+//       message={['horrible', 'bad', 'ok', 'good', 'super']}
+//     />
+//     <Test />
+//     <StarRating maxRating={10} defaultRating={2} />
+//   </React.StrictMode>
+// );
 
-  return (
-    <div>
-      <StarRating maxRating={7} onMovieRating={setMovieRating} />
-      <p>this movie is a {movieRating} star rating</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating maxRating={7} onMovieRating={setMovieRating} />
+//       <p>this movie is a {movieRating} star rating</p>
+//     </div>
+//   );
+// }
