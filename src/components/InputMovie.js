9cb507669246f4160a5fe26ca73301b6757bf629
Joary13 <jolea13008@gmail.com>
2023-12-22 11:50:02 +0100

    useRef to focus on search when enter pressed

--- src/components/InputMovie.js
+++ src/components/InputMovie.js
@@ -1,5 +1,29 @@
+import { useEffect, useRef } from 'react';
+
 // stateful component
 export function InputMovie({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function handleFocus(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === 'Enter') {
+          setQuery('');
+          inputEl.current.focus();
+        }
+      }
+      document.addEventListener('keydown', handleFocus);
+      return function () {
+        document.removeEventListener('keydown', handleFocus);
+      };
+    },
+    [setQuery]
+  );
+  // useEffect(function () {
+  //   const el = document.querySelector('.search');
+  //   el.focus();
+  // }, []);
   return (
     <input
       className='search'
@@ -7,6 +31,7 @@ export function InputMovie({ query, setQuery }) {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
